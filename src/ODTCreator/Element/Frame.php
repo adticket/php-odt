12cd19414fabf9536d2f68d9c2bff77fb67f3876
Jan Unger <ju@juit.de>
2013-08-11 00:49:42 +0200

    Dynamic frame position, value object for length dimensions

--- src/ODTCreator/Element/Frame.php
+++ src/ODTCreator/Element/Frame.php
@@ -3,9 +3,26 @@
 namespace ODTCreator\Element;
 
 use ODTCreator\Document\Content as ContentFile;
+use ODTCreator\Value\Length;
 
 class Frame implements Element
 {
+    /**
+     * @var Length
+     */
+    private $x;
+
+    /**
+     * @var Length
+     */
+    private $y;
+
+    public function __construct(Length $x, Length $y)
+    {
+        $this->x = $x;
+        $this->y = $y;
+    }
+
     /**
      * @var Element[]
      */
@@ -27,11 +44,11 @@ class Frame implements Element
     public function renderTo(\DOMDocument $domDocument, \DOMElement $parentElement = null)
     {
         $frameElement = $domDocument->createElementNS(ContentFile::NAMESPACE_DRAW, 'draw:frame');
-        $frameElement->setAttributeNS(ContentFile::NAMESPACE_DRAW, 'draw:style-name', 'Frame');
+        $frameElement->setAttributeNS(ContentFile::NAMESPACE_DRAW, 'draw:style-name', 'Frame-1');
         $frameElement->setAttributeNS(ContentFile::NAMESPACE_TEXT, 'text:anchor-type', 'page');
         $frameElement->setAttributeNS(ContentFile::NAMESPACE_TEXT, 'text:anchor-page-number', '1');
-        $frameElement->setAttributeNS(ContentFile::NAMESPACE_SVG, 'svg:x', '2cm');
-        $frameElement->setAttributeNS(ContentFile::NAMESPACE_SVG, 'svg:y', '2.7cm');
+        $frameElement->setAttributeNS(ContentFile::NAMESPACE_SVG, 'svg:x', $this->x->getValue());
+        $frameElement->setAttributeNS(ContentFile::NAMESPACE_SVG, 'svg:y', $this->y->getValue());
         $frameElement->setAttributeNS(ContentFile::NAMESPACE_SVG, 'svg:width', '8.5cm');
         $frameElement->setAttributeNS(ContentFile::NAMESPACE_SVG, 'svg:height', '4.5cm');
         $frameElement->setAttributeNS(ContentFile::NAMESPACE_DRAW, 'draw:z-index', '0');
---
+++ src/ODTCreator/Value/Length.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace ODTCreator\Value;
+
+class Length
+{
+    /**
+     * @var string
+     */
+    private $value;
+
+    public function __construct($value)
+    {
+        $this->assertIsValid($value);
+        $this->value = $value;
+    }
+
+    private function assertIsValid($value)
+    {
+        if (!preg_match('/^\d+(\.\d+){0,1}cm$/', $value)) {
+            throw new \InvalidArgumentException("Unrecognized length value '$value'");
+        }
+    }
+
+    public function getValue()
+    {
+        return $this->value;
+    }
+}
