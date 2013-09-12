ee3aedac33fa7272a56e5fbf84bd2b73262dc4b7
Jan Unger <ju@juit.de>
2013-09-12 14:31:41 +0200

    Repaired and refactored example

--- example/example.php
+++ example/example.php
@@ -1,12 +1,11 @@
 <?php
 
 use ODTCreator\Content\LineBreak;
+use ODTCreator\Content\Text;
 use ODTCreator\Element\Frame;
 use ODTCreator\Element\Paragraph;
-use ODTCreator\Content\Text;
-use ODTCreator\Style\Factory;
+use ODTCreator\ODTCreator;
 use ODTCreator\Style\TextStyle;
-use ODTCreator\Value\Color;
 use ODTCreator\Value\FontSize;
 use ODTCreator\Value\Length;
 use OdtToPdfRenderer\InstantRenderer;
@@ -14,159 +13,243 @@ use PDFToPNGRenderer\PDFToPNGRenderer;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$outputDir = __DIR__ . '/output';
-$odtFile = new SplFileInfo($outputDir . '/hello_world.odt');
 
-//exec("rm -fr $outputDir");
-//mkdir($outputDir);
+class ExampleBuilder
+{
+    private $styleFactory;
+    /**
+     * @var SplFileinfo
+     */
+    private $outputDirInfo;
 
+    /**
+     * @var ODTCreator
+     */
+    private $odtCreator;
 
-$dummyText = file_get_contents(__DIR__ . '/dummyText.txt');
-
-// Create an ODT file
-$startTime = microtime(true);
-$odt = new \ODTCreator\ODTCreator();
-
-
-// Adresse
-
-$frame = new Frame('Frame-1', new Length('2cm'), new Length('2.7cm'), new Length('8.5cm'), new Length('4.5cm'));
-$p = new Paragraph('FP');
-$p->addContent(new Text('AD ticket GmbH'));
-$p->addContent(new LineBreak());
-$p->addContent(new Text('Herr Dinko Bicvic'));
-$p->addContent(new LineBreak());
-$p->addContent(new Text('Kaiserstr. 69'));
-$p->addContent(new LineBreak());
-$p->addContent(new LineBreak());
-$p->addContent(new Text('60329 Frankfurt am Main'));
-
-$frame->addSubElement($p);
-$odt->addElement($frame);
-
-
-// Datum
-
-$frame = new Frame('Frame-1', new Length('13cm'), new Length('8cm'), new Length('8cm'), new Length('2cm'));
-$p = new Paragraph();
-$p->addContent(new Text('Frankfurt, den ' . date('d.m.Y')));
-$frame->addSubElement($p);
-$odt->addElement($frame);
-
-
-// Betreff
-
-$p = new Paragraph('P1');
-$style = new TextStyle('T123');
-$style->setBold();
-$text = new Text('Ihr neues Serienbriefmodul', $style);
-$p->addContent($text);
-$odt->addElement($p);
-
-
-// Anrede
-
-$p = new Paragraph();
-$p->addContent(new LineBreak());
-$p->addContent(new LineBreak());
-$p->addContent(new Text('Sehr geehrter Herr Bicvic,'));
-$p->addContent(new LineBreak());
-$odt->addElement($p);
-
-
-// Inhalt
-
-//$styleFactory = new Factory();
-//$paragraphStyle = $styleFactory->createParagraphStyle();
-//$p = new Paragraph($paragraphStyle);
-$p = new Paragraph();
-
-// Convert line breaks
-
-$dummyText = str_replace("\r\n", "\n", $dummyText);
-$dummyText = str_replace("\r", "\n", $dummyText);
-$lines = explode("\n", $dummyText);
-$isFirstLine = true;
-foreach ($lines as $line) {
-    if ($isFirstLine) {
-        $isFirstLine = false;
-    } else {
-        $p->addContent(new LineBreak());
+    public function __construct(\SplFileinfo $outputDirInfo)
+    {
+        $this->outputDirInfo = $outputDirInfo;
+        $this->odtCreator = new ODTCreator();
+        $this->styleFactory = $this->odtCreator->getStyleFactory();
     }
-    if ('' !== $line) {
-        $p->addContent(new Text($line));
+
+    public function build()
+    {
+        $this->cleanUp();
+        $odtFileInfo = $this->createOdtFile();
+        $pdfFileInfo = $this->renderPdf($odtFileInfo);
+        $this->renderPngs($pdfFileInfo);
+    }
+
+    private function cleanUp()
+    {
+        if ($this->outputDirInfo->isDir()) {
+            system("rm -fr {$this->outputDirInfo->getPathname()}");
+        }
+        mkdir($this->outputDirInfo->getPathname());
+    }
+
+    /**
+     * @return SplFileInfo
+     */
+    private function createOdtFile()
+    {
+        $this->addAddressFrame();
+        $this->addDateFrame();
+        $this->addSubject();
+        $this->addSalutation();
+        $this->addContent();
+        $this->addRegards();
+
+        // Render to ODT
+        $odtFileInfo = new SplFileInfo($this->outputDirInfo->getPathname() . '/hello_world.odt');
+        $this->odtCreator->save($odtFileInfo);
+
+        $unzipDir = substr($odtFileInfo->getPathname(), 0, -4);
+        system("rm -fr {$unzipDir}");
+        system("unzip {$odtFileInfo->getPathname()} -d {$unzipDir}");
+
+        $this->validateOdtFile($odtFileInfo);
+
+        return $odtFileInfo;
+    }
+
+    private function addAddressFrame()
+    {
+        $frameStyle = $this->styleFactory->createGraphicStyle();
+        $textStyle = $this->createDefaultTextStyle();
+
+        $xCoordinate = new Length('2cm');
+        $yCoordinate = new Length('2.7cm');
+        $width = new Length('8.5cm');
+        $height = new Length('4.5cm');
+        $addressFrame = new Frame($frameStyle, $xCoordinate, $yCoordinate, $width, $height);
+
+        $paragraph = new Paragraph();
+        $paragraph->addContent(new Text('Mustermann GmbH', $textStyle));
+        $paragraph->addContent(new LineBreak());
+        $paragraph->addContent(new Text('Herr Max Mustermann', $textStyle));
+        $paragraph->addContent(new LineBreak());
+        $paragraph->addContent(new Text('Musterstr. 1', $textStyle));
+        $paragraph->addContent(new LineBreak());
+        $paragraph->addContent(new LineBreak());
+        $paragraph->addContent(new Text('12345 Musterstadt', $textStyle));
+
+        $addressFrame->addSubElement($paragraph);
+        $this->odtCreator->addElement($addressFrame);
+    }
+
+    /**
+     * @return TextStyle
+     */
+    private function createDefaultTextStyle()
+    {
+        $textStyle = $this->styleFactory->createTextStyle();
+        $textStyle->setFontSize(new FontSize('11pt'));
+        $textStyle->setFontName('Arial');
+
+        return $textStyle;
+    }
+
+    private function addDateFrame()
+    {
+        $frameStyle = $this->styleFactory->createGraphicStyle();
+
+        $xCoordinate = new Length('13cm');
+        $yCoordinate = new Length('8cm');
+        $width = new Length('8cm');
+        $height = new Length('2cm');
+        $dateFrame = new Frame($frameStyle, $xCoordinate, $yCoordinate, $width, $height);
+
+        $paragraph = new Paragraph();
+        $content = new Text('Musterdorf, den ' . date('d.m.Y'), $this->createDefaultTextStyle());
+        $paragraph->addContent($content);
+
+        $dateFrame->addSubElement($paragraph);
+        $this->odtCreator->addElement($dateFrame);
+    }
+
+    private function addSubject()
+    {
+        $paragraphStyle = $this->styleFactory->createParagraphStyle();
+
+        $paragraphStyle->setMasterPageName('First_20_Page');
+        $paragraphStyle->setMarginBottom(new Length('24pt'));
+        $paragraph = new Paragraph($paragraphStyle);
+
+        $textStyle = $this->createDefaultTextStyle();
+        $textStyle->setBold();
+        $text = new Text('Ihr Schreiben', $textStyle);
+
+        $paragraph->addContent($text);
+        $this->odtCreator->addElement($paragraph);
+    }
+
+    private function addSalutation()
+    {
+        $paragraph = new Paragraph($this->createDefaultParagraphStyle());
+
+        $paragraph->addContent(new Text('Sehr geehrter Herr Mustermann,', $this->createDefaultTextStyle()));
+
+        $this->odtCreator->addElement($paragraph);
+    }
+
+    /**
+     * @return \ODTCreator\Style\ParagraphStyle
+     */
+    private function createDefaultParagraphStyle()
+    {
+        $paragraphStyle = $this->styleFactory->createParagraphStyle();
+        $paragraphStyle->setMarginBottom(new Length('12pt'));
+
+        return $paragraphStyle;
+    }
+
+    private function addContent()
+    {
+        $dummyText = $this->getDummyText();
+        $paragraphs = $this->createParagraphs($dummyText);
+
+        foreach ($paragraphs as $paragraph) {
+            $this->odtCreator->addElement($paragraph);
+        }
+    }
+
+    /**
+     * @return string
+     */
+    private function getDummyText()
+    {
+        return 'überall dieselbe alte Leier. Das Layout ist fertig, der Text lässt auf sich warten.
+Damit das Layout nun nicht nackt im Raume steht und sich klein und leer vorkommt, springe ich ein: der Blindtext. Genau zu diesem Zwecke erschaffen, immer im Schatten meines großen Bruders »Lorem Ipsum«, freue ich mich jedes Mal, wenn Sie ein paar Zeilen lesen. Denn esse est percipi - Sein ist wahrgenommen werden. Und weil Sie nun schon die Güte haben, mich ein paar weitere Sätze lang zu begleiten, möchte ich diese Gelegenheit nutzen, Ihnen nicht nur als Lückenfüller zu dienen, sondern auf etwas hinzuweisen, das es ebenso verdient wahrgenommen zu werden: Webstandards nämlich.
+Sehen Sie, Webstandards sind das Regelwerk, auf dem Webseiten aufbauen. So gibt es Regeln für HTML, CSS, JavaScript oder auch XML; Worte, die Sie vielleicht schon einmal von Ihrem Entwickler gehört haben. Diese Standards sorgen dafür, dass alle Beteiligten aus einer Webseite den größten Nutzen ziehen. Im Gegensatz zu früheren Webseiten müssen wir zum Beispiel nicht mehr zwei verschiedene Webseiten für den Internet Explorer und einen anderen Browser programmieren. Es reicht eine Seite, die - richtig angelegt - sowohl auf verschiedenen Browsern im Netz funktioniert, aber ebenso gut für den Ausdruck oder die Darstellung auf einem Handy geeignet ist. Wohlgemerkt: Eine Seite für alle Formate. Was für eine Erleichterung. Standards sparen Zeit bei den Entwicklungskosten und sorgen dafür, dass sich Webseiten später leichter pflegen lassen. Natürlich nur dann, wenn sich alle an diese Standards halten. Das gilt für Browser wie Firefox, Opera, Safari und den Internet Explorer ebenso wie für die Darstellung in Handys.
+Und was können Sie für Standards tun? Fordern Sie von Ihren Designern und Programmieren einfach standardkonforme Webseiten. Ihr Budget wird es Ihnen auf Dauer danken. Ebenso möchte ich Ihnen dafür danken, dass Sie mich bin zum Ende gelesen haben. Meine Mission ist erfüllt. Ich werde hier noch die Stellung halten, bis der geplante Text eintrifft. Ich wünsche Ihnen noch einen schönen Tag. Und arbeiten Sie nicht zuviel!';
+    }
+
+    /**
+     * @param string $dummyText
+     * @return Paragraph[]
+     */
+    private function createParagraphs($dummyText)
+    {
+        $paragraphs = array();
+        $textBlocks = explode("\n", $dummyText);
+        $defaultTextStyle = $this->createDefaultTextStyle();
+        $paragraphStyle = $this->createDefaultParagraphStyle();
+        foreach ($textBlocks as $text) {
+            $paragraph = new Paragraph($paragraphStyle);
+            $paragraph->addContent(new Text($text, $defaultTextStyle));
+            $paragraphs[] = $paragraph;
+        }
+
+        return $paragraphs;
+    }
+
+    private function addRegards()
+    {
+        $paragraph = new Paragraph();
+        $paragraph->addContent(new Text('Mit freundlichen Grüßen', $this->createDefaultTextStyle()));
+
+        $this->odtCreator->addElement($paragraph);
+    }
+
+    /**
+     * @param SplFileInfo $odtFileInfo
+     */
+    private function validateOdtFile(SplFileInfo $odtFileInfo)
+    {
+        system(
+            "cd " . __DIR__ . "/../tests/ODTCreator/Test/EndToEnd/odf-validator && "
+            . "./validator --file=" . $odtFileInfo->getPathname()
+        );
+    }
+
+    /**
+     * @param SplFileInfo $odtFileInfo
+     * @return \SplFileInfo
+     */
+    private function renderPdf(SplFileInfo $odtFileInfo)
+    {
+        $libreOfficeBinary = new SplFileInfo('/Applications/LibreOffice.app/Contents/MacOS/soffice');
+        $pdfFileInfo = new SplFileInfo($this->outputDirInfo->getPathname() . '/example.pdf');
+        $pdfRenderer = new InstantRenderer($libreOfficeBinary);
+        $pdfRenderer->render($odtFileInfo, $pdfFileInfo);
+
+        return $pdfFileInfo;
+    }
+
+    /**
+     * @param SplFileInfo $pdfFileInfo
+     */
+    private function renderPngs(SplFileInfo $pdfFileInfo)
+    {
+        $ghostscriptBinary = new SplFileInfo('/usr/local/bin/gs');
+        $pngRenderer = new PDFToPNGRenderer($ghostscriptBinary);
+        $pngFileInfos = $pngRenderer->render($pdfFileInfo, 20, 22);
     }
 }
 
-//    $textStyle = new TextStyle('t' . $i);
-//
-//    switch ($i % 4) {
-//        case 0:
-//            $color = '#ff0000';
-//            break;
-//        case 1:
-//            $color = '#00ff00';
-//            break;
-//        case 2:
-//            $color = '#0000ff';
-//            break;
-//        default:
-//            $color = '#000000';
-//            break;
-//    }
-//    $textStyle->setColor(new Color($color));
-//    $textStyle->setBold();
-//    $textStyle->setFontSize(new FontSize(($i * 2) + 6));
-//    $odt->addTextStyle($textStyle);
-
-for ($i = 0; $i < 4; $i++) {
-    $odt->addElement($p);
-}
-
-
-// Grußformel
-
-$p = new Paragraph();
-$p->addContent(new Text('Mit freundlichen Grüßen'));
-$odt->addElement($p);
-
-
-// Render to ODT
-
-$odt->save($odtFile);
-$renderTimeODT = microtime(true) - $startTime;
-
-$unzipDir = substr($odtFile->getPathname(), 0, -4);
-system("rm -fr {$unzipDir}");
-system("unzip {$odtFile->getPathname()} -d {$unzipDir}");
-
-system("cd " . __DIR__ . "/../tests/ODTCreator/Test/EndToEnd/odf-validator && ./validator --file=" . $odtFile->getPathname());
-
-
-// Render ODT file to PDF file
-
-$startTime = microtime(true);
-$libreOfficeBinary = new SplFileInfo('/Applications/LibreOffice.app/Contents/MacOS/soffice');
-$pdfRenderer = new InstantRenderer($libreOfficeBinary);
-$pdfFileInfo = $pdfRenderer->render($odtFile);
-
-var_dump($pdfFileInfo->getPathname());
-$renderTimePDF = microtime(true) - $startTime;
-
-
-// Render PDF pages to PNG files
-
-$startTime = microtime(true);
-$ghostscriptBinary = new SplFileInfo('/usr/local/bin/gs');
-$pngRenderer = new PDFToPNGRenderer($ghostscriptBinary);
-$pngFileInfos = $pngRenderer->render($pdfFileInfo, 20, 22);
-
-foreach ($pngFileInfos as $pngFileInfo) {
-    var_dump($pngFileInfo->getPathname());
-}
-$renderTimePNG = microtime(true) - $startTime;
-
-
-echo "Rendering ODT: " . $renderTimeODT . " msec\n";
-echo "Rendering PDF: " . $renderTimePDF . " msec\n";
-echo "Rendering PNG: " . $renderTimePNG . " msec\n";
+$outputDirInfo = new SplFileInfo(__DIR__ . '/output');
+$exampleBuilder = new ExampleBuilder($outputDirInfo);
+$exampleBuilder->build();
