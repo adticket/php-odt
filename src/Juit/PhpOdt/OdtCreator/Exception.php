6f34f1acbb9d05fe28b0cb9cbb373271aa375e36
Jan Unger <ju@juit.de>
2014-05-02 14:21:14 +0200

    Removed dead code

--- src/Juit/PhpOdt/OdtCreator/Exception.php
+++ src/Juit/PhpOdt/OdtCreator/Exception.php
@@ -3,20 +3,4 @@
 namespace Juit\PhpOdt\OdtCreator;
 
 class Exception extends \Exception
-{
-    protected $message;
-
-    function __construct($message)
-    {
-        $this->message = $message;
-    }
-
-    function __toString()
-    {
-        $class = new \ReflectionClass($this);
-        $trace = $this->getTrace();
-        $errorMsg = 'Exception "' . $class->getName() . '" in ' . $trace[0]['file'] . '(' . $trace[0]['line'] . ')' .
-            ': <strong>' . $this->getMessage() . '</strong>';
-        return $errorMsg;
-    }
-}
+{}
