8c8d7778e4cfd7bb138f657eee482b5f806ee654
Christian Nielebock <christian.nielebock@adticket.de>
2017-09-05 12:27:47 +0200

    - added renderer for direct calling soffice binary instead of unoconv

---
+++ src/Juit/PhpOdt/OdtToPdfRenderer/SofficeRenderer.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Juit\PhpOdt\OdtToPdfRenderer;
+
+use SplFileInfo;
+
+/**
+ * Class SofficeRenderer
+ *
+ * @package Juit\PhpOdt\OdtToPdfRenderer
+ */
+class SofficeRenderer extends AbstractOdtToPdfRenderer
+{
+    /**
+     * @var string
+     */
+    private $libreOfficeBinaryPath;
+
+    /**
+     * SofficeRenderer constructor.
+     *
+     * @param SplFileInfo|null $backgroundPdfPath
+     * @param SplFileInfo|null $stampPdfPath
+     * @param string|null      $libreOfficeBinaryPath
+     */
+    public function __construct(
+        SplFileInfo $backgroundPdfPath = null,
+        SplFileInfo $stampPdfPath = null,
+        $libreOfficeBinaryPath = null
+    ) {
+        parent::__construct($backgroundPdfPath, $stampPdfPath);
+
+        if (null === $libreOfficeBinaryPath) {
+            $libreOfficeBinaryPath = '/usr/bin/soffice';
+        }
+        $this->libreOfficeBinaryPath = $libreOfficeBinaryPath;
+    }
+
+    /**
+     * @param \SplFileInfo $odtFile
+     * @return string
+     */
+    protected function createShellCommand(\SplFileInfo $odtFile)
+    {
+        return $this->libreOfficeBinaryPath .
+            '--headless --convert-to pdf ' .
+            $odtFile->getPathname() .
+            '--outdir ' . $odtFile->getPath();
+    }
+}
